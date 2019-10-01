d7e218cea160989a92066bea40a658aed506ef23
Marcos Alvira Romero <marcos.alvira@iesdoctorbalmis.com>
2019-10-01 12:54:32 +0200

    Cambiar Trailer

--- Reproductor (Star Wars IX)/MainWindow.xaml.cs
+++ Reproductor (Star Wars IX)/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Reproductor__Star_Wars_IX_
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
